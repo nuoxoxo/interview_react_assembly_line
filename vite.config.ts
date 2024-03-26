71ffb551ac2246bd8d8f1e0a775d97cd7817f9c0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-26 20:17:53 +0100

    assemblyline/deploy

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+*/
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/interview_react_assembly_line/'
+  }
+
+  return config
+})
